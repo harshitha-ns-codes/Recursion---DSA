2ba8acfe8fbbe772cdc92a92166e43eb4c31c3ca
Harshitha N <n.harshitha1003@gmail.com>
2026-03-07 06:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.42%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        long long N = n;
+        double ans = 1.0;
+        if(N<0)
+        {
+            N = -N;
+        }
+
+        while(N>0)
+        {
+        if(N%2 ==1 )
+        {
+            ans = ans*x;
+            N = N-1;
+        }
+        else
+        {
+            N = N/2;
+            x = x*x;
+        }
+
+        
+        }
+        if(n < 0) 
+        {
+        ans =  1.0/ans;
+        }
+        return ans;
+        
+    }
+};
