d985a718e0005942060459af9b515bacba1d9ae8
Harshitha N <n.harshitha1003@gmail.com>
2026-04-22 15:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (96.51%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void func(int sum,int last,vector<int> &nums,vector<vector<int>>&ans, int k)
+    {
+        if(sum == 0 && nums.size() == k)
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        if(sum <= 0 ||nums.size() > k ) return;
+
+        for(int i = last ; i<= 9 ; i++)
+        {
+            if(i<=sum)
+            {
+                nums.push_back(i);
+                func(sum - i , i+1 , nums , ans , k);
+                nums.pop_back();
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) 
+    {
+        vector<int> nums;
+        vector<vector<int>> ans;
+        func(n , 1 , nums , ans , k);
+        return ans;
+        
+    }
+};
